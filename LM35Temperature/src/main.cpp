301b477b6d836ebfc18a89efb90b3e596039a3a3
ChilloutGeek <adamgemenez@gmail.com>
2023-04-11 22:46:11 +0800

    Serial Version

--- LM35Temperature/src/main.cpp
+++ LM35Temperature/src/main.cpp
@@ -2,16 +2,19 @@
 
 const int LM35_pin = A1;
 const float LM35_calib = 0.48875; //convert voltage to temp
+char temperature;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   // put your setup code here, to run once:
 }
 
 void loop() {
   float temperature = analogRead(LM35_pin) * LM35_calib;
+  char temp_str[10];
+  dtostrf(temperature, 4, 2, temp_str);
   Serial.println(temperature);
-  delay(1000);
+  delay(5000);
 
   // put your main code here, to run repeatedly:
 }
--- scripts/sensor.py
+++ scripts/sensor.py
@@ -1,15 +1,25 @@
+import paho.mqtt.client as mqtt
 import requests
 import serial
 import time
 
-url = 'http://localhost:8000/api/data/temperature/'
+ser = serial.Serial('/dev/ttyACM1', 115200, timeout=1)
+mqtt_server = 'localhost'
+mqtt_port = 1883
+mqtt_topic = 'temperature'
+rest_url = 'http://localhost:8000/api/data/temperature/'
 
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+def on_connect(client, userdata, flags,rc):
+    print('Connected to MQTT broker with result code' + str(rc))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.connect(mqtt_server, mqtt_port, 60)
 
 while True:
-    temperature = ser.readline().decode('utf-8').strip()
-    payload = {'temp':temperature}
-    response = requests.post(url, data=payload)
-    print(response.text)
-
-    time.sleep(5)
+    if ser.in_waiting > 0:
+        temperature = ser.readline().decode('utf-8').strip()
+        temp_json = json.dumps{('temp':temperature, ensure_ascii=False, separators=(',':':'))}
+        client.publish(mqtt_topic, temp_json)
+        response = requests.post(rest_url, data={'temp':temp_json})
+        time.sleep(8)
